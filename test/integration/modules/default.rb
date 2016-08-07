ae537a04d6eb584c0e41076eeff49b49547557ac
Michal Kubenka <mkubenka@gmail.com>
2016-08-07 08:44:01 +0200

    Fix test on CentOS 6. Default GeoIP package on CentOS 6 is using some older database.

--- test/integration/modules/default.rb
+++ test/integration/modules/default.rb
@@ -1,5 +1,5 @@
 # Check with AWS US IP.
-describe command('curl -H "X-Forwarded-For: 52.192.0.1" -I localhost/block') do
+describe command('curl -H "X-Forwarded-For: 129.33.160.1" -I localhost/block') do
     its('stdout') { should match /200 OK/ }
     its('exit_status') { should eq 0 }
 end
